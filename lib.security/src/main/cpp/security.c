ba432e479ae849c1f534fb5b236a98ac4edfb6f6
suli <sss908938889@163.com>
2017-07-20 00:51:01 +0800

    Update

--- lib.security/src/main/cpp/security.c
+++ lib.security/src/main/cpp/security.c
@@ -2,4 +2,43 @@
 // Created by 苏力 on 2017/7/19.
 //
 
-#include "security.h"
+#include <jni.h>
+#include <sys/types.h>
+
+#define TARGET_CLASS "com/suli/lib/utils/EncryptUtils"
+
+
+jbyteArray encryptAES(JNIEnv *env, jbyteArray input, jbyteArray key) {
+
+}
+
+
+static const JNINativeMethod gMethods[] = {
+        {"encryptAES", "([B[B)[B", (void *) encryptAES}
+};
+
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
+    JNIEnv *env = NULL;
+    if ((*vm)->GetEnv(vm, (void **) &env, JNI_VERSION_1_4) != JNI_OK) {
+        return -1;
+    }
+
+    jclass clazz = (*env)->FindClass(env, TARGET_CLASS);
+    if (!clazz) {
+        return -1;
+    }
+    //这里就是关键了，把本地函数和一个java类方法关联起来。不管之前是否关联过，一律把之前的替换掉！
+    if ((*env)->RegisterNatives(env, clazz, gMethods,
+                                sizeof(gMethods) / sizeof(gMethods[0])) != JNI_OK) {
+        return -1;
+    }
+
+    return JNI_VERSION_1_4;
+}
+
+JNIEXPORT void JNICALL JNI_OnUnload(JavaVM *vm, void *reserved) {
+    JNIEnv *env = NULL;
+    if ((*vm)->GetEnv(vm, (void **) &env, JNI_VERSION_1_4) != JNI_OK) {
+        return;
+    }
+}
